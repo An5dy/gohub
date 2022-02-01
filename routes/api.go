d8888ab782b805acedba4cfa64ff441137acf370
an5dy <846562014@qq.com>
2022-02-02 00:44:24 +0800

    手机是否注册接口

--- routes/api.go
+++ routes/api.go
@@ -1,7 +1,7 @@
 package routes
 
 import (
-	"net/http"
+	"gohub/app/http/controllers/api/v1/auth"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,10 +10,11 @@ func RegisterAPIRoutes(r *gin.Engine) {
 	v1 := r.Group("v1")
 	{
 		// 注册路由
-		v1.GET("/", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"Hello": "Wolrd!",
-			})
-		})
+		authGroup := v1.Group("/auth")
+		{
+			suc := new(auth.SignupController)
+			// 判断手机是否已注册
+			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
+		}
 	}
 }
