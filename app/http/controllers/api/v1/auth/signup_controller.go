9f74335272ab8d24d533e510b9f826c986714d13
an5dy <846562014@qq.com>
2022-02-06 23:47:57 +0800

    JWT 授权

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -4,6 +4,7 @@ import (
 	v1 "gohub/app/http/controllers/api/v1"
 	"gohub/app/models/user"
 	"gohub/app/requests"
+	"gohub/pkg/jwt"
 	"gohub/pkg/response"
 
 	"github.com/gin-gonic/gin"
@@ -57,8 +58,10 @@ func (s *SignupController) SignupUsingPhone(c *gin.Context) {
 	userModel.Create()
 
 	if userModel.ID > 0 {
+		token := jwt.NewJWT().IssueToken(userModel.GetStringID(), userModel.Name)
 		response.CreatedJSON(c, gin.H{
-			"data": userModel,
+			"data":  userModel,
+			"token": token,
 		})
 	} else {
 		response.Abort500(c, "创建用户失败, 请稍后尝试~")
@@ -82,8 +85,10 @@ func (s *SignupController) SignupUsingEmail(c *gin.Context) {
 	userModel.Create()
 
 	if userModel.ID > 0 {
+		token := jwt.NewJWT().IssueToken(userModel.GetStringID(), userModel.Name)
 		response.CreatedJSON(c, gin.H{
-			"data": userModel,
+			"data":  userModel,
+			"token": token,
 		})
 	} else {
 		response.Abort500(c, "创建用户失败, 请稍后尝试~")
