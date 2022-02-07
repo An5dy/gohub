1983f6889716bb90b02802b1d89cdf23b92ec4c5
an5dy <846562014@qq.com>
2022-02-07 21:50:56 +0800

    限流中间件

--- routes/api.go
+++ routes/api.go
@@ -2,15 +2,22 @@ package routes
 
 import (
 	"gohub/app/http/controllers/api/v1/auth"
+	"gohub/app/http/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterAPIRoutes(r *gin.Engine) {
 	v1 := r.Group("v1")
+
+	// 全局限流中间件：每小时限流。这里是所有 API （根据 IP）请求加起来。
+	// 作为参考 Github API 每小时最多 60 个请求（根据 IP）。
+	// 测试时，可以调高一点。
+	v1.Use(middlewares.LimitIP("200-H"))
 	{
 		// 注册认证路由
 		authGroup := v1.Group("/auth")
+		authGroup.Use(middlewares.LimitIP("1000-H"))
 		{
 			suc := new(auth.SignupController)
 			// 判断手机是否已注册
@@ -24,11 +31,11 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			// 发送验证码
 			vcc := new(auth.VerifyCodeController)
 			// 图片验证码，需要加限流
-			authGroup.POST("/verify-codes/captcha", vcc.ShowCaptcha)
+			authGroup.POST("/verify-codes/captcha", middlewares.LimitPerRoute("20-H"), vcc.ShowCaptcha)
 			// 发送手机验证码
-			authGroup.POST("/verify-codes/phone", vcc.SendUsingPhone)
+			authGroup.POST("/verify-codes/phone", middlewares.LimitPerRoute("20-H"), vcc.SendUsingPhone)
 			// 发送邮件验证码
-			authGroup.POST("/verify-codes/email", vcc.SendUsingEmail)
+			authGroup.POST("/verify-codes/email", middlewares.LimitPerRoute("20-H"), vcc.SendUsingEmail)
 			// 登录
 			lc := new(auth.LoginController)
 			// 使用手机号，短信验证码进行登录
