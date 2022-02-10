aec8378d0da864c8c609e57ecc229cf2228987c9
an5dy <846562014@qq.com>
2022-02-10 16:23:52 +0800

    migrate fresh 命令

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -2,7 +2,9 @@ package database
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
+	"gohub/pkg/config"
 
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -30,4 +32,54 @@ func Connect(dbConfig gorm.Dialector, _logger logger.Interface) {
 	if err != nil {
 		fmt.Println(err.Error())
 	}
+}
+
+// 获取当前数据库名
+func CurrentDatabase() (dbname string) {
+	dbname = DB.Migrator().CurrentDatabase()
+	return
+}
+
+func DeleteAllTables() error {
+	var err error
+
+	switch config.Get("database.connection") {
+	case "mysql":
+		deleteMysqlDatabase()
+	case "sqlite":
+		deleteAllSqliteTables()
+	default:
+		panic(errors.New("database connection not supported"))
+	}
+
+	return err
+}
+
+// 删除所有 sqlite 中的表
+func deleteAllSqliteTables() error {
+	tables := []string{}
+	DB.Select(&tables, "SELECT name FROM sqlite_master WHERE type='table'")
+	for _, table := range tables {
+		DB.Migrator().DropTable(table)
+	}
+	return nil
+}
+
+// 删除所有 mysql 中当前数据库的表
+func deleteMysqlDatabase() error {
+	dbname := CurrentDatabase()
+	sql := fmt.Sprintf("DROP DATABASE %s;", dbname)
+	if err := DB.Exec(sql).Error; err != nil {
+		return err
+	}
+
+	sql = fmt.Sprintf("CREATE DATABASE %s;", dbname)
+	if err := DB.Exec(sql).Error; err != nil {
+		return err
+	}
+	sql = fmt.Sprintf("USE %s;", dbname)
+	if err := DB.Exec(sql).Error; err != nil {
+		return err
+	}
+	return nil
 }
