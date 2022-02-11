3932f23c54569e30007b89b7fd3d18627d575434
an5dy <846562014@qq.com>
2022-02-12 01:12:34 +0800

    编辑个人资料

--- app/http/controllers/api/v1/users_controller.go
+++ app/http/controllers/api/v1/users_controller.go
@@ -33,3 +33,21 @@ func (uc *UsersController) Index(c *gin.Context) {
 		"pager": pager,
 	})
 }
+
+func (uc *UsersController) UpdateProfile(c *gin.Context) {
+	request := requests.UserRequest{}
+	if ok := requests.Validate(c, &request, requests.UserSave); !ok {
+		return
+	}
+
+	currentUser := auth.CurrentUser(c)
+	currentUser.Name = request.Name
+	currentUser.City = request.City
+	currentUser.Introduction = request.Introduction
+	rowsAffected := currentUser.Save()
+	if rowsAffected > 0 {
+		response.Data(c, currentUser)
+	} else {
+		response.Abort500(c, "更新失败, 请稍后尝试~")
+	}
+}
